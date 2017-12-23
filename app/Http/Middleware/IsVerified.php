3f5cc2a40c559929b44407a51ce968c9094a33c6
ibtesamrauf <irkibby@gmail.com>
2017-12-23 15:43:08 +0500

    settings for email varification

--- app/Http/Middleware/IsVerified.php
+++ app/Http/Middleware/IsVerified.php
@@ -19,7 +19,7 @@ class IsVerified
     {
         if(!auth()->user()->verified){
             Session::flush();
-            return redirect('/login')->withAlert('Please verify your email before login.');
+            return redirect('login')->withAlert('Please verify your email before login.');
         }
         return $next($request);
     }
