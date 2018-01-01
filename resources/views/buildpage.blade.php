36ee77f52a0dad85f99683f7935d23b6f82a13da
ibtesamrauf <irkibby@gmail.com>
2018-01-01 19:01:19 +0500

    change register to create page tag on buildpage blade

--- resources/views/buildpage.blade.php
+++ resources/views/buildpage.blade.php
@@ -107,7 +107,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    Create Page
                                 </button>
                             </div>
                         </div>
