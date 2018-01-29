c3b9dee8b4c4944744b728401e5c22cc3e4acb6b
ibtesamrauf <irkibby@gmail.com>
2018-01-29 17:11:53 +0500

    user migrations edit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');   
+            $table->string('last_name');
+            $table->string('profile_picture' , 500);
             $table->string('user_role');
             $table->string('company_name');
             $table->string('email', 200)->unique();
+            $table->integer('company_id');
+            $table->string('phone_number');
+            $table->string('country');
+            $table->string('title');
+            $table->string('faebook_url');
+            $table->string('instagram_url');
+            $table->string('youtube_url');
+            $table->string('twitter_url');
+            $table->string('soundcloud_url');
+            $table->string('website_blog');
+            $table->string('monthly_visitors');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
