eb5a29ff1dcf8211552f0a97622e8071661d4a8e
ibtesamrauf <irkibby@gmail.com>
2018-02-06 14:46:46 +0500

    get last value of country in registertion fails the validation

--- resources/views/auth/register_2.blade.php
+++ resources/views/auth/register_2.blade.php
@@ -126,7 +126,7 @@
                                 <select name="country" class="form-control" id="country">
                                     <option>Select</option>
                                     @foreach($country as $country_value)
-                                        <option value="{{ $country_value->id }}">{{ $country_value->country_name }}</option>
+                                        <option value="{{ $country_value->id }}" @if (old('country') == $country_value->id) selected="selected" @endif>{{ $country_value->country_name }}</option>
                                     @endforeach
                                 </select>
                                 @if ($errors->has('country'))
