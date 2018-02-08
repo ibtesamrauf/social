1825a157fbec396c324d724dd0c86f4a9e4ff900
ibtesamrauf <irkibby@gmail.com>
2018-02-08 11:47:28 +0500

    search page done one thing lest

--- app/Http/Controllers/FindInfulencerController.php
+++ app/Http/Controllers/FindInfulencerController.php
@@ -9,6 +9,9 @@ use Auth;
 use App\User_page;
 use App\Country;
 use App\Preferred_medium;
+use App\Facebook_page_data;
+use App\Instagram_page_data;
+use App\Youtube_page_data;
 
 class FindInfulencerController extends Controller
 {
@@ -49,12 +52,65 @@ class FindInfulencerController extends Controller
         return view('findinfluencer',compact('user_page_data'));
     }
 
-    public function finde_influencer_test()
+    public function finde_influencer_test(Request $request)
     {
         // vv("asdasd");
+        $search_page_data = array();
+        $pageLimit = 25;
+        if(isset($request->search)){
+            $user_page_data = User_page::with('Users')
+                        ->with('Facebook_page')
+                        ->with('Youtube_page')
+                        ->where('page_title', 'like' , "%$request->search%")
+                        ->orWhere('page_description', 'like' , "%$request->search%")
+                        ->orWhere('page_about_your_self', 'like' , "%$request->search%")
+                        ->paginate($pageLimit);   
+            if($user_page_data->isEmpty()){
+                $user_page_data->search = "No match found. Search again!";
+            }                   
+        }else{
+            $user_page_data = User_page::with('Users')->with('Facebook_page')->with('Youtube_page')->paginate($pageLimit);          
+        }
+        
+//                1190547
+        if(isset($request->likes_on_Facebook)){
+            if($request->likes_on_Facebook_checkbox == 1){
+                $facebook_data = Facebook_page_data::where('likes' , '>=' ,  $request->likes_on_Facebook)
+                                        ->paginate($pageLimit);                
+                $search_page_data = $facebook_data;
+                // vv($search_page_data);
+            }
+        }
+
+        if(isset($request->followers_on_Instagram)){
+            if($request->followers_on_Instagram_checkbox == 1){
+                $instagram_data = Instagram_page_data::where('followed_by' , '>=' ,  $request->followers_on_Instagram)->get();
+
+                foreach ($instagram_data as $key => $value) {
+                    $search_page_data->push($value);
+                }
+            }
+        }
+
+
+        if(isset($request->subscribers_on_Youtube)){
+            if($request->subscribers_on_Youtube_checkbox == 1){
+                // v($request->subscribers_on_Youtube);
+                $youtube_data = Youtube_page_data::where('subscriberCount' , '>=' ,  $request->subscribers_on_Youtube)->get();
+                // vv($youtube_data);
+                foreach ($youtube_data as $key => $value) {
+                    $search_page_data->push($value);
+                }
+            }
+        }
+
+        
+
+        // vv($search_page_data);
+        
         $preferred_medium_value = Preferred_medium::get();
         $country = Country::orderBy('id' , 'asc')->get();
-        return view('finde_influencer_test' , compact('country' , 'preferred_medium_value'));
+        return view('finde_influencer_test' , compact('country' , 'preferred_medium_value' , 'search_page_data'));
     }
 
     public function upload_youtube_video()
--- resources/views/finde_influencer_test.blade.php
+++ resources/views/finde_influencer_test.blade.php
@@ -3,16 +3,6 @@
 @section('content')
 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script type="text/javascript">
-    $(document).ready(function () {
-        $("#add_another").click(function () {
-            console.log("asdasd");
-            $("#div_criteria").append('<br>Traction of likes:<input type="text" class="form-control" placeholder="Enter number of likes">From:<select id="country" name="country" class="form-control"><option>likes on Facebook</option><option>followers on Twitter</option><option>followers on Instagram</option><option>Subscribers on Youtube</option><option>Followers on Soundcloud</option><option>Blog visitors per month</option></select>');
-        });
-    });
-</script>
-
-
 
 <div class="main">
     <section class="module bg-dark-30" data-background="assets/images/section-4.jpg">
@@ -37,7 +27,7 @@
                                 {{ session('status') }}
                             </div>
                             @endif
-                            <form action="/findinfulencer"  method="GET">
+                            <form action="/finde_influencer_test"  method="GET">
                                 <div class="form-group">
                                     <label for="exampleInputEmail1">Keyword Search</label>
                                     <?php
@@ -53,7 +43,7 @@
 
                             </form>
                             <br><br>
-                            <!-- <form action="/findinfulencer"  method="GET"> -->
+                            <form action="/finde_influencer_test"  method="GET"> 
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Advanced Search</label>
 
@@ -66,10 +56,10 @@
                                         <th style="width: 10%;">Action</th>
                                         <tr>
                                             <!-- <td><input type="checkbox"></td> -->
-                                            <td><input type="text" class="form-control" id="likes_on_Facebook" placeholder="Enter number of likes"></td>
-                                            <td><input type="checkbox"> on Facebook</td>
+                                            <td><input type="number" class="form-control" name="likes_on_Facebook" id="likes_on_Facebook" value="{{ Input::get('likes_on_Facebook') ? $_GET['likes_on_Facebook'] : ''}}" placeholder="Enter number of likes"></td>
+                                            <td><input type="checkbox" {{ Input::get('likes_on_Facebook_checkbox') == 1 ? 'checked' : ''}} name="likes_on_Facebook_checkbox" value="1" id="likes_on_Facebook_checkbox" > on Facebook</td>
                                             <td>
-                                                <select name="action">
+                                                <select name="action_facebook">
                                                     <option value="1">AND</option>
                                                     <option value="2">OR</option>
                                                 </select>
@@ -80,24 +70,10 @@
 
                                         <tr>
                                             <!-- <td><input type="checkbox"></td> -->
-                                            <td><input type="text" class="form-control" id="followers_on_Twitter" placeholder="Enter number of followers "></td>
-                                            <td><input type="checkbox"> on Twitter</td>
+                                            <td><input type="number" class="form-control" name="followers_on_Instagram" id="followers_on_Instagram" value="{{ Input::get('followers_on_Instagram') ? $_GET['followers_on_Instagram'] : ''}}" placeholder="Enter number of followers"></td>
+                                            <td><input type="checkbox" {{ Input::get('followers_on_Instagram_checkbox') == 1 ? 'checked' : ''}} name="followers_on_Instagram_checkbox" value="1" id="followers_on_Instagram_checkbox"> on Instagram</td>
                                             <td>
-                                                <select name="action">
-                                                    <option value="1">AND</option>
-                                                    <option value="2">OR</option>
-                                                </select>
-    <!--                                             <input name="action" type="radio">or&nbsp
-                                                <input name="action" type="radio">and -->
-                                            </td>
-                                        </tr>
-
-                                        <tr>
-                                            <!-- <td><input type="checkbox"></td> -->
-                                            <td><input type="text" class="form-control" id="followers_on_Instagram" placeholder="Enter number of followers"></td>
-                                            <td><input type="checkbox"> on Instagram</td>
-                                            <td>
-                                                <select name="action">
+                                                <select name="action_instagram">
                                                     <option value="1">AND</option>
                                                     <option value="2">OR</option>
                                                 </select>
@@ -109,10 +85,10 @@
                                         <tr>
                                             <!-- <td><input type="checkbox"></td> -->
 
-                                            <td><input type="text" class="form-control" id="subscribers_on_Youtube" placeholder="Enter number of Subscribers"></td>
-                                            <td><input type="checkbox"> on Youtube</td>
+                                            <td><input type="number" class="form-control" name="subscribers_on_Youtube" id="subscribers_on_Youtube" value="{{ Input::get('subscribers_on_Youtube') ? $_GET['subscribers_on_Youtube'] : ''}}" placeholder="Enter number of Subscribers"></td>
+                                            <td><input type="checkbox" {{ Input::get('subscribers_on_Youtube_checkbox') == 1 ? 'checked' : ''}} name="subscribers_on_Youtube_checkbox" value="1" id="subscribers_on_Youtube_checkbox"> on Youtube</td>
                                             <td>
-                                                <select name="action">
+                                                <select name="action_youtube">
                                                     <option value="1">AND</option>
                                                     <option value="2">OR</option>
                                                 </select>
@@ -123,12 +99,26 @@
 
                                         <tr>
                                             <!-- <td><input type="checkbox"></td> -->
+                                            <td><input type="number" class="form-control" name="followers_on_Twitter" id="followers_on_Twitter" value="{{ Input::get('followers_on_Twitter') ? $_GET['followers_on_Twitter'] : ''}}"  placeholder="Enter number of followers "></td>
+                                            <td><input type="checkbox" {{ Input::get('followers_on_Twitter_checkbox') == 1 ? 'checked' : ''}} name="followers_on_Twitter_checkbox" value="1" id="followers_on_Twitter_checkbox"> on Twitter</td>
+                                            <td>
+                                                <select name="action_twitter">
+                                                    <option value="1">AND</option>
+                                                    <option value="2">OR</option>
+                                                </select>
+    <!--                                             <input name="action" type="radio">or&nbsp
+                                                <input name="action" type="radio">and -->
+                                            </td>
+                                        </tr>
 
-                                            <td><input type="text" class="form-control" id="followers_on_Soundcloud" placeholder="Enter number of Followers"></td>
-                                            <td><input type="checkbox"> on Soundcloud</td>
+                                        <tr>
+                                            <!-- <td><input type="checkbox"></td> -->
+
+                                            <td><input type="number" class="form-control" name="followers_on_Soundcloud" id="followers_on_Soundcloud" value="{{ Input::get('followers_on_Soundcloud') ? $_GET['followers_on_Soundcloud'] : ''}}" placeholder="Enter number of Followers"></td>
+                                            <td><input type="checkbox" {{ Input::get('followers_on_Soundcloud_checkbox') == 1 ? 'checked' : ''}} name="followers_on_Soundcloud_checkbox" value="1" id="followers_on_Soundcloud_checkbox"> on Soundcloud</td>
 
                                             <td>
-                                                <select name="action">
+                                                <select name="action_soundcloud">
                                                     <option value="1">AND</option>
                                                     <option value="2">OR</option>
                                                 </select>
@@ -140,11 +130,11 @@
                                         <tr>
                                             <!-- <td><input type="checkbox"></td> -->
 
-                                            <td><input type="text" class="form-control" id="Blog_visitors_per_month" placeholder="Enter number of "></td>
-                                            <td><input type="checkbox"> Blog visitors per month</td>
+                                            <td><input type="number" class="form-control" name="Blog_visitors_per_month" id="Blog_visitors_per_month" value="{{ Input::get('Blog_visitors_per_month') ? $_GET['Blog_visitors_per_month'] : ''}}" placeholder="Enter number of "></td>
+                                            <td><input type="checkbox" {{ Input::get('Blog_visitors_per_month_checkbox') == 1 ? 'checked' : ''}} name="Blog_visitors_per_month_checkbox" value="1" id="Blog_visitors_per_month_checkbox"> Blog visitors per month</td>
 
                                             <td>
-                                                <select name="action">
+                                                <select name="action_blogs">
                                                     <option value="1">AND</option>
                                                     <option value="2">OR</option>
                                                 </select>
@@ -199,7 +189,7 @@
                             </div>
                             <button type="submit" class="btn btn-primary">Advance Search</button>
 
-                            <!-- </form> -->
+                            </form> 
 
 
                         </div>
@@ -212,39 +202,65 @@
                                     <!-- <a href="/" class="btn btn-primary">Go back to home</a> -->
 
                                     <table class="table">
-                                    <!-- <th>User Name</th> -->
+                                        <th>S.no</th>
                                         <th>Page title</th>
-                                        <th>About Them</th>
                                         <th>Facebook Likes</th>
-                                        <th>Youtube Subscriber</th>
+                                        <th>Image</th>
+                                        
                                         <th>Add to favorite</th>
                                         <th>Message</th>
-                                        <tr>
+                                        <?php 
+                                        if(!empty($search_page_data)){
+                                        ?>
 
-                                            <td>
-                                                page_title 
-                                            </td>
-                                            <td>
-                                                page_about_your_self
-                                            </td>
-                                            <td>
-                                                Facebook_page likes
-                                            </td>
-                                            <td>
-                                                Youtube_page subscriberCount
-                                            </td>
-                                            <td>
-                                                <a href="#">
-                                                    <span class="glyphicon glyphicon-star"></span>
-                                                </a>
-                                            </td>
-                                            <td>
-                                                <a href="#">
-                                                    <span class="glyphicon glyphicon-envelope"></span>
-                                                </a>
-                                            </td>
-                                        </tr>
+                                        @foreach($search_page_data as $key => $data)
+                                            <tr>        
+                                                <td>
+                                                    {{ $key + 1 }}
+                                                </td>
+                                                <td>
+                                                    <?php 
+                                                        if(isset($data->name)){
+                                                            echo $data->name; 
+                                                        }
+                                                    ?> 
+                                                </td>
+                                                <td>
+                                                    <?php 
+                                                        if(isset($data->likes)){
+                                                            echo $data->likes; 
+                                                        }else if (isset($data->followed_by)) {
+                                                            echo $data->followed_by;
+                                                        }elseif (isset($data->subscriberCount)) {
+                                                            echo $data->subscriberCount;
+                                                        }
+                                                    ?> 
+                                                </td>
+                                                <td>
+                                                    <img src="{{ isset($data->image) ? $data->image : ''}}" style=" max-width: 190px; " class="img-thumbnail">
+                                                </td>
+                                                
+                                                <td>
+                                                    <a href="#">
+                                                        <span class="glyphicon glyphicon-star"></span>
+                                                    </a>
+                                                </td>
+                                                <td>
+                                                    <a href="#">
+                                                        <span class="glyphicon glyphicon-envelope"></span>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        <?php
+                                        }else{
+                                            echo "Nothing";
+                                        }
+                                        ?>
                                     </table>
+                                    <?php if(!empty($search_page_data)){ ?>
+                                        <div class="pagination-wrapper"> {!! $search_page_data->appends(Input::except('page'))->render() !!} </div>
+                                    <?php } ?>
                                 </div>
                             </div>
                         </div>
