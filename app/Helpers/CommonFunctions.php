740cbd58ebb062824c92e0858f648b231a08ef0c
ibtesamrauf <irkibby@gmail.com>
2018-02-27 16:22:30 +0500

    auth error in commonfunction.php file

--- app/Helpers/CommonFunctions.php
+++ app/Helpers/CommonFunctions.php
@@ -8,12 +8,12 @@ class CommonFunctions {
     public static function newMessageCount($user_type)
     {
         if($user_type == 'marketer'){
-            return \App\Participant_marketer::where('to_user' , Auth::user()->id)
+            return \App\Participant_marketer::where('to_user' , \Auth::user()->id)
                                         ->where('user_type' , $user_type)
                                         ->where('unread' , 1)
                                         ->count();
         }else{
-            return \App\Participant_marketer::where('to_user' , Auth::guard('jobseeker')->user()->id)
+            return \App\Participant_marketer::where('to_user' , \Auth::guard('jobseeker')->user()->id)
                                         ->where('user_type' , $user_type)
                                         ->where('unread' , 1)
                                         ->count();
