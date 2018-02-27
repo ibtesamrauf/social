e1719a40d299d061028557a4855ed106fbda79a4
ibtesamrauf <irkibby@gmail.com>
2018-02-27 16:12:51 +0500

    un commit message count

--- app/Helpers/CommonFunctions.php
+++ app/Helpers/CommonFunctions.php
@@ -7,17 +7,16 @@ class CommonFunctions {
 	// \App\Helpers\CommonFunctions
     public static function newMessageCount($user_type)
     {
-        // if($user_type == 'marketer'){
-        //     return \App\Participant_marketer::where('to_user' , Auth::user()->id)
-        //                                 ->where('user_type' , $user_type)
-        //                                 ->where('unread' , 1)
-        //                                 ->count();
-        // }else{
-        //     return \App\Participant_marketer::where('to_user' , Auth::guard('jobseeker')->user()->id)
-        //                                 ->where('user_type' , $user_type)
-        //                                 ->where('unread' , 1)
-        //                                 ->count();
-        // }
-        return 1;
+        if($user_type == 'marketer'){
+            return \App\Participant_marketer::where('to_user' , Auth::user()->id)
+                                        ->where('user_type' , $user_type)
+                                        ->where('unread' , 1)
+                                        ->count();
+        }else{
+            return \App\Participant_marketer::where('to_user' , Auth::guard('jobseeker')->user()->id)
+                                        ->where('user_type' , $user_type)
+                                        ->where('unread' , 1)
+                                        ->count();
+        }
     }
 }
