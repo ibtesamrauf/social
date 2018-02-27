e3460c0a83b625694ef9d94f1023996927be1815
ibtesamrauf <irkibby@gmail.com>
2018-02-27 15:36:03 +0500

    add a common function file1

---
+++ app/Helpers/CommonFunctions.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Helpers;
+
+
+class CommonFunctions {
+	// \App\Helpers\CommonFunctions
+    public static function newMessageCount($user_type)
+    {
+        // if($user_type == 'marketer'){
+        //     return \App\Participant_marketer::where('to_user' , Auth::user()->id)
+        //                                 ->where('user_type' , $user_type)
+        //                                 ->where('unread' , 1)
+        //                                 ->count();
+        // }else{
+        //     return \App\Participant_marketer::where('to_user' , Auth::guard('jobseeker')->user()->id)
+        //                                 ->where('user_type' , $user_type)
+        //                                 ->where('unread' , 1)
+        //                                 ->count();
+        // }
+        return 1;
+    }
+}
