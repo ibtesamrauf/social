53b35ceb5c22d78c5fa48a881704f543553a260d
ibtesamrauf <irkibby@gmail.com>
2018-03-13 14:13:24 +0500

    user table migration when colum exixt do nothing if not make it

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,28 +15,91 @@ class CreateUsersTable extends Migration
     {
         if(!Schema::hasTable('users')){
             Schema::create('users', function (Blueprint $table) {
-                $table->increments('id');
-                $table->string('first_name');   
-                $table->string('last_name');
-                $table->string('profile_picture' , 500);
-                $table->string('user_role');
-                $table->string('company_name');
-                $table->string('email', 200)->unique();
-                // $table->string('email');
-                $table->integer('company_id');
-                $table->string('phone_number');
-                $table->string('country');
-                $table->string('title');
-                $table->string('faebook_url');
-                $table->string('instagram_url');
-                $table->string('youtube_url');
-                $table->string('twitter_url');
-                $table->string('soundcloud_url');
-                $table->string('website_blog');
-                $table->string('monthly_visitors');
-                $table->string('password');
-                $table->string('provider');
-                $table->string('provider_id');
+                if(!Schema::hasColumn('users', 'id'))
+                {
+                    $table->increments('id');
+                }
+                if(!Schema::hasColumn('users', 'first_name'))
+                {
+                    $table->string('first_name');   
+                }
+                if(!Schema::hasColumn('users', 'last_name'))
+                {
+                    $table->string('last_name');
+                }
+                if(!Schema::hasColumn('users', 'profile_picture'))
+                {
+                    $table->string('profile_picture' , 500);
+                }
+                if(!Schema::hasColumn('users', 'user_role'))
+                {
+                    $table->string('user_role');
+                }
+                if(!Schema::hasColumn('users', 'company_name'))
+                {
+                    $table->string('company_name');
+                }
+                if(!Schema::hasColumn('users', 'email'))
+                {
+                    $table->string('email', 200)->unique();
+                }
+                if(!Schema::hasColumn('users', 'company_id'))
+                {
+                    $table->integer('company_id');   
+                }
+                if(!Schema::hasColumn('users', 'phone_number'))
+                {
+                    $table->string('phone_number');
+                }
+                if(!Schema::hasColumn('users', 'country'))
+                {
+                    $table->string('country');
+                }
+                if(!Schema::hasColumn('users', 'title'))
+                {
+                    $table->string('title');
+                }
+                if(!Schema::hasColumn('users', 'faebook_url'))
+                {
+                    $table->string('faebook_url');
+                }
+                if(!Schema::hasColumn('users', 'instagram_url'))
+                {
+                    $table->string('instagram_url');
+                }
+                if(!Schema::hasColumn('users', 'youtube_url'))
+                {
+                    $table->string('youtube_url');
+                }
+                if(!Schema::hasColumn('users', 'twitter_url'))
+                {
+                    $table->string('twitter_url');
+                }
+                if(!Schema::hasColumn('users', 'soundcloud_url'))
+                {
+                    $table->string('soundcloud_url');
+                }
+                if(!Schema::hasColumn('users', 'website_blog'))
+                {
+                    $table->string('website_blog');
+                }
+                if(!Schema::hasColumn('users', 'monthly_visitors'))
+                {
+                    $table->string('monthly_visitors');
+                }
+                if(!Schema::hasColumn('users', 'password'))
+                {
+                    $table->string('password');   
+                }
+                if(!Schema::hasColumn('users', 'provider'))
+                {
+                    $table->string('provider');
+                }
+
+                if(!Schema::hasColumn('users', 'provider_id'))
+                {
+                    $table->string('provider_id');
+                }
                 $table->rememberToken();
                 $table->timestamps();
             });
