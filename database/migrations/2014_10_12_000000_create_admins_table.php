12ba81f5149f5fc4166b0b4eb95eb008e29a2a8d
ibtesamrauf <irkibby@gmail.com>
2018-03-14 12:41:32 +0500

    add provider and provider_id in admin table

--- database/migrations/2014_10_12_000000_create_admins_table.php
+++ database/migrations/2014_10_12_000000_create_admins_table.php
@@ -19,25 +19,13 @@ class CreateAdminsTable extends Migration
                 $table->string('first_name');   
                 $table->string('last_name');
                 $table->string('profile_picture' , 500);
-                // $table->string('user_role');
-                // $table->string('company_name');
                 $table->string('email', 200)->unique();
-                // $table->integer('company_id');
                 $table->string('phone_number');
-                $table->string('country');
-                // $table->string('title');
-                // $table->string('faebook_url');
-                // $table->string('instagram_url');
-                // $table->string('youtube_url');
-                // $table->string('twitter_url');
-                // $table->string('soundcloud_url');
-                // $table->string('website_blog');
-                // $table->string('monthly_visitors');
-                
-                
+                $table->string('country');    
                 $table->tinyInteger('verified')->default(0);
                 $table->string('verification_token')->nullable();
-
+                $table->string('provider');
+                $table->string('provider_id');
                 $table->string('password');
                 $table->rememberToken();
                 $table->timestamps();
